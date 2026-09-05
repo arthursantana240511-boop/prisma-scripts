88ffd885207deb3865827ccfb23aed9d2125d8b4
arthursantana240511-boop <arthursantana240511@gmail.com>
2026-09-05 11:54:02 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 /* EDITE SOMENTE ESTA LISTA PARA ADICIONAR SEUS SCRIPTS. */
 const SCRIPTS=[
- {slug:'delta',name:'script roube um ovo',description:'Seu primeiro script do Prisma Scripts.',icon:'⚡',link:'https://raw.githubusercontent.com/Abdullahking20/loader-lua/main/loader'},
+ {slug:'delta',name:'script-roube um ovo',description:'Seu primeiro script do Prisma Scripts.',icon:'⚡',link:'https://raw.githubusercontent.com/Abdullahking20/loader-lua/main/loader'},
  {slug:'blox-fruits',name:'Blox Fruits',description:'Script para seu conteúdo de Blox Fruits.',icon:'◆',link:'https://example.com'},
  {slug:'mm2',name:'Murder Mystery 2',description:'Adicione aqui a descrição do seu script.',icon:'◈',link:'https://example.com'}
 ];
